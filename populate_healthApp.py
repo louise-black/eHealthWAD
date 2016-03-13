57f2cc3cebe437f4ab017b7a7121dd5427d0ad3a
Μαρία Παπαδοπούλου < >
2016-03-13 22:01:24 +0000

    populate script

---
+++ populate_healthApp.py
@@ -0,0 +1,75 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+
+import django
+django.setup()
+
+from rango.models import Category, Page
+
+
+def populate():
+    asthma_cat = add_cat('Asthma')
+
+    add_page(cat=asthma_cat,
+        title="Asthma - NHS Choices",
+        url="http://www.nhs.uk/conditions/asthma/Pages/Introduction.aspx/")
+
+    add_page(cat=asthma_cat,
+        title="Asthma: MedlinePlus",
+        url="https://www.nlm.nih.gov/medlineplus/asthma.html/")
+
+    add_page(cat=asthma_cat,
+        title="healthfinder.gov - Prevent Allergy and Asthma Attacks at Home",
+        url="http://healthfinder.gov/HealthTopics/Category/parenting/safety/prevent-allergy-and-asthma-attacks-at-home/")
+
+    cancer_cat = add_cat("Cancer")
+
+    add_page(cat=cancer_cat,
+        title="Cancer Research",
+        url="http://www.cancerresearchuk.org/")
+
+    add_page(cat=cancer_cat,
+        title="MedlinePlus - Cancer",
+        url="https://vsearch.nlm.nih.gov/vivisimo/cgi-bin/query-meta?v%3Aproject=medlineplus&v%3Asources=medlineplus-bundle&query=cancer")
+
+    add_page(cat=cancer_cat,
+        title="healthfinder.gov - Health Topics - Health Conditions and Diseases - Cancer",
+        url="http://healthfinder.gov/HealthTopics/Category/health-conditions-and-diseases/cancer/")
+
+    diabetes_cat = add_cat("Diabetes")
+
+    add_page(cat=diabetes_cat,
+        title="Diabetes UK – Care. Connect. Campaign. - Diabetes UK",
+        url="https://www.diabetes.org.uk/")
+
+    add_page(cat=diabetes_cat,
+        title="MedlinePlus - Diabetes",
+        url="https://vsearch.nlm.nih.gov/vivisimo/cgi-bin/query-meta?v%3Aproject=medlineplus&v%3Asources=medlineplus-bundle&query=diabetes/")
+
+    add_page(cat=diabetes_cat,
+        title="healthfinder.gov - Preventing Diabetes: Questions for the doctor",
+        url="http://healthfinder.gov/HealthTopics/Category/doctor-visits/talking-with-the-doctor/preventing-diabetes-questions-for-the-doctor/")
+
+
+    # Print out what we have added to the user.
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print "- {0} - {1}".format(str(c), str(p))
+
+def add_page(cat, title, url, views=0, likes=0):
+        p.views=views
+        
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url=url
+    
+    p.save()
+    return p
+
+def add_cat(name):
+    c = Category.objects.get_or_create(name=name)[0]
+    return c
+
+# Start execution here!
+if __name__ == '__main__':
+    print "Starting healthApp population script..."
+    populate()
