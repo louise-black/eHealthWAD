f8406b82972eb0b833568b327e09b05a0a8f9e79
RebeccaEEMartin <rebyk511@msn.com>
2016-03-14 20:05:13 +0000

    changed forms and models

--- healthApp/forms.py
+++ healthApp/forms.py
@@ -29,6 +29,9 @@ class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    readability = forms.IntegerField()
+    subjectivity = forms.IntegerField()
+    polarity = forms.IntegerField()
 
     class Meta:
         # Provide an association between the ModelForm and a model
@@ -53,6 +56,33 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
 
         return cleaned_data
+
+    def process(self):
+        html = urllib.urlopen(url).read()
+        soup = BeautifulSoup(html)
+
+        # kill all script and style elements
+        for script in soup(["script", "style"]):
+            script.extract()    # rip it out
+
+        # get text
+        text = soup.get_text()
+
+        # break into lines and remove leading and trailing space on each
+        lines = (line.strip() for line in text.splitlines())
+        # break multi-headlines into a line each
+        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        # drop blank lines
+        text = '\n'.join(chunk for chunk in chunks if chunk)
+
+        blob = TextBlob(text)
+        sent = blob.sentiment
+        subj = 100 - int((sent.subjectivity)*100)                     #the less subjective, the better
+        polar = 100 - int((sent.polarity)*100)                        #the less polar, the better
+        readability = int(textstat.flesch_reading_ease(text))
+
+        return subj, polar, readability
+
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
