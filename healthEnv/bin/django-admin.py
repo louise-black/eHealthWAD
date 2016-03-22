ab0e5feb3b0fa09d084573d23c436670c0135d80
Louise Black <2129047B@student.gla.ac.uk>
2016-03-22 21:11:28 +0000

    remove accidentally added virtual environment

--- healthEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/louiseblack/Documents/uni/wadProject/eHealthWAD/healthEnv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
