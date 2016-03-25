5aeef731ad6bc0794803fded84bbc822c22c6591
Louise Black <2129047B@student.gla.ac.uk>
2016-03-25 21:47:07 +0000

    fixed population script

--- populate_healthApp.py
+++ populate_healthApp.py
@@ -6,42 +6,26 @@ from healthApp.models import Category, Page, UserProfile
 from django.contrib.auth.models import User
 
 def populate():
-    add_user(username='jill',
-             password = 'jill',
-             dob = "1988-01-03",
-             email = 'jill@gmail.com',
-             forename = 'Jill',
-             surname = 'Jill',
-             gender = 'F'
-             )
-    add_user (username = 'jim',
-              password = 'jim',
-              dob = "1996-02-04",
-              email = 'jim@gmail.com',
-              forename = 'Jim',
-              surname = 'Jim',
-              gender = 'M')
-    add_user (username = 'joe',
-              password = 'joe',
-              dob = "1990-05-22",
-              email = 'joe@gmail.com',
-              forename = 'Joe',
-              surname = 'Joe',
-              gender = 'M')
-def add_user(username, password, dob, email, forename, surname, gender):
-    u = UserProfile.objects.get_or_create(
-        #username = username,
-        #password = password,
-        dob = dob,
-        #email = email,
-        forename = forename,
-        surname = surname,
-        gender = gender)[0]
-    u.save()
-    u.user.username = username
-    u.user.password = password
-    u.user.email = email
-    return u
+    bob_user = add_user('bob','bob',"1988-01-03",'bob@gmail.com','Bob','Bob','F')
+    jim_user = add_user ('jim','jim',"1996-02-04",'jim@gmail.com','Jim','Jim','M')
+    joe_user = add_user ('joe','joe',"1990-05-22",'joe@gmail.com','Joe','Joe','M')
+def add_user(uSername, pAssword, dob, email, forename, surname, gender):
+        try:
+    		u = User.objects.get(username=uSername)
+    		up = UserProfile(user=u)
+        except:
+            u = User(username=uSername)
+            u.set_password(pAssword)
+            u.save()
+            up = UserProfile(user=u)
+            up.dob = dob
+            up.email = email
+            up.forename = forename
+            up.surname = surname
+            up.gender = gender
+            up.save()
+        return up
+
 # Start execution here!
 if __name__ == '__main__':
     print "Starting healthApp population script..."
